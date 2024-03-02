9452dedc25156db60e6da0e955d7130203453b6f
mr-hariom-chaudhary <mr.hariomchaudhary@gmail.com>
2024-03-02 14:11:30 +0530

    update

--- java practise file/Addition.java
+++ java practise file/Addition.java
@@ -2,10 +2,12 @@ import java.util.*;
 
 public class Addition{
     public static void main(String args[]) {
-        int x, y, sum,p,i;
+        int x, y, sum;
+
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the first number: ");
         x = sc.nextInt();
+        
         System.out.print("Enter the second number: ");
         y = sc.nextInt();
         sum = sum(x, y);
