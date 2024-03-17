25124db661ad4de867bf2767e7250a68ce2428a5
mr-hariom-chaudhary <mr.hariomchaudhary@gmail.com>
2024-03-17 17:37:24 +0530

    java

--- java practise file/Operator/patterns/assignment_pattern1.java
+++ java practise file/Operator/patterns/assignment_pattern1.java
@@ -1,23 +1,18 @@
 public class assignment_pattern1 {
-public static void main(String[] args) {
-    for(int i=4;i>=1;i--)
-    {
-        for(int j=1;j<=i;j++)
-        {
-            System.out.print("* ");
-        }
-            System.out.println();
-    }
-    {
-        for(int i=4;i>=1;i--)
-        {
-            for(int j=1;j<=i;j++)
-            {
+    public static void main(String[] args) {
+        for (int i = 4; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
             }
+            System.out.println();
+        }
+        {
+            for (int i = 4; i >= 1; i--) {
+                for (int j = 1; j <= i; j++) {
+                    System.out.print("* ");
+                }
                 System.out.println();
+            }
         }
     }
-    }
 }
-
